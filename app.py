3a8b587bb5752841bb2424a7fedd17ac3fac0413
Luis2001Goncalves <95137322+Luis2001Goncalves@users.noreply.github.com>
2024-07-11 10:54:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
+
 @app.route('/')
 def hello_world():
     app.logger.info('Serving Hello World!')
     return 'Olá Mundo!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
